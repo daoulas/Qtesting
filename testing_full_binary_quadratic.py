1c31cc10eb8e886a3b60ec48dde9591de5dbbd07
Kostas Daoulas <daoulas@mpip-mainz.mpg.de>
2022-09-20 14:31:31 +0000

    wrote example with exact solver

--- testing_full_binary_quadratic.py
+++ testing_full_binary_quadratic.py
@@ -1,3 +1,9 @@
+# This exercise searches for a minimum of a binary quadratic model obtained by "expanding" the compact form 
+# J*sum_i={1,N-1} (1-s(i))*(1-s(i+1)), where s(i), i=1...N are binary variables. The goal of the exercise 
+# is to understand how to use  the definition via dimod.BinaryQuadraticModel. 
+
+
+# Uses exact solver
 
 from dwave.system import DWaveSampler,EmbeddingComposite
   
@@ -22,18 +28,64 @@ for i in range(2,N):
 
 offset = J*(N-1)
 
-print(Q)
+#print(Q)
 
-print(V)
+#print(V)
 
-print(offset)
+#print(offset)
 
 vartype = dimod.BINARY
 
 model = dimod.BinaryQuadraticModel(V, Q, offset, vartype)
 
+#first use the exact solver which enumerates all possible states
+
 exactsolver = dimod.ExactSolver()
 
 results = exactsolver.sample(model)
 
-print(results)
+length = len(results)
+
+l = 0
+
+#unsorted output 
+
+while l<length:
+   print(results.record[l][0], results.record[l][1], results.record[l][2])
+   l = l+1
+
+#print(results)
+
+# We can directly consider the "compact" form J*sum_i={1,N-1} t(i)*t(i+1), where we substituted t(i) = 1 - s(i).
+# The results should be the same to those we obtained from the expanded form (keeping  in mind that s(i) = 1 -t(i)
+
+for i in range(1,N):
+       Q[i,i+1] = J
+
+for i in range(1,N+1):
+       V[i] = 0
+
+offset = 0
+
+model = dimod.BinaryQuadraticModel(V,Q,offset,vartype)
+
+exactsolver = dimod.ExactSolver()
+
+results = exactsolver.sample(model)
+
+print ('=========================')
+
+length = len(results)
+
+l = 0
+
+#unsorted output
+
+while  l<length:
+   print(1 - results.record[l][0], results.record[l][1], results.record[l][2])
+   l = l+1
+
+#print(results)
+
+
+
