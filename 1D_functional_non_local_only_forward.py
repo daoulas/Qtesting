6bd8ec255caf9b25106a689b8e72b3ddce2298c4
Kostas Daoulas <daoulas@mpip-mainz.mpg.de>
2023-02-23 06:58:13 +0000

    corrected error in 1D_fun...._only_forward

--- 1D_functional_non_local_only_forward.py
+++ 1D_functional_non_local_only_forward.py
@@ -1,8 +1,11 @@
+
+# This program tests how D-Wave handles the minimization of a square-gradient term defined via a simple forward difference scheme:
+# F = (Kappa*D/2)*\Sum_{i=0}^{nodes-1}[(p(i+1)-p(i))/D]^2
+# Periodic boundary conditions apply: p(nodes) = p(0) and p(-1) = p(nodes-1)
+
 from dwave.system import DWaveSampler,EmbeddingComposite
   
 import dimod, math
-
-# import greedy
 
 import dwave.inspector
 
@@ -12,45 +15,35 @@ V = {}
 
 density_result = {}
 
-# number of variables. We use 3-bit a approximation, so each lattice node has 5 binary variables for our functional. 
-# q_k[i] k = 1 .. 5 and i = 0 .. n indicates the node.  In total we have n + 1 nodes. We map q_k[i] on a 1-D matrix
-# q[k+i*5]
+# number of variables. We use 3-bit a approximation, so each lattice node has 3 binary variables for our functional. 
+# q_k[i] k = 1 .. 3 and i = 0 .. n indicates the node.  In total we have n + 1 nodes. We map q_k[i] on a 1-D matrix
+# q[k+i*3]
 
 nodes = 5 
+
+runs = 5000
 
 #largest value of p
 
 h = 1
 
-#reference value of p
-
-po = 1
-
-#chemical potential
-
-chempot = 0 
-
 #step of lattice in space 
 D = 1
 
-u = 5.
-
-D2 = D*D 
 #
-po2 = po*po
+Kappa = 5.
 
-Kappa = 15.
+Coeff = (Kappa/2.)*(1/D)  
 
-Coeff = (Kappa/2.)*(1/D2)
+# units of p[i]: ~1/Length
+# units of Kappa: energy*Length^3
+# units of Coeff: energy*Length^2 
 
 print("Coeff = ", Coeff)
 
 b1 = h/7
 b2 = 2*h/7
 b3 = 4*h/7
-
-#Weight coeffs for constraints
-Lan2 = 3.5
 
 #define powers 
 
@@ -67,11 +60,10 @@ b3p3 = math.pow(b3,3)
 b3p4 = math.pow(b3,4)
 
 for i in range(0,nodes+1):
-      Q[1+i*3,2+i*3] =  2.*b1*b2*Coeff 
-      Q[1+i*3,3+i*3] =  2.*b1*b3*Coeff 
-      Q[2+i*3,3+i*3] =  2.*b2*b3*Coeff 
+      Q[1+i*3,2+i*3] =  4.*b1*b2*Coeff 
+      Q[1+i*3,3+i*3] =  4.*b1*b3*Coeff 
+      Q[2+i*3,3+i*3] =  4.*b2*b3*Coeff 
      
-
 for i in range(0,nodes): 
      Q[1+i*3,1+(i+1)*3] = -2*b1p2*Coeff
      Q[1+i*3,2+(i+1)*3] = -2*b1*b2*Coeff
@@ -94,18 +86,9 @@ Q[3+0*3,2+nodes*3] = -2*b2*b3*Coeff
 Q[3+0*3,3+nodes*3] = -2*b3p2*Coeff
 
 for i in range(0,nodes+1):
-        V[1+i*3] = Coeff*b1p2
-        V[2+i*3] = Coeff*b2p2
-        V[3+i*3] = Coeff*b3p2
-
-#print("Coupling coefficients")
-
-#for i in range(1,6):
-#    for j in range(i+1,6):
-#        print(i, j, Q[i,j])
- 
-#for i in range(1,6):
-#    print(i,V[i])
+        V[1+i*3] = Coeff*2.*b1p2
+        V[2+i*3] = Coeff*2.*b2p2
+        V[3+i*3] = Coeff*2.*b3p2
 
 offset = 0
 
@@ -115,24 +98,70 @@ model = dimod.BinaryQuadraticModel(V, Q, offset, vartype)
 
 sampler = EmbeddingComposite(DWaveSampler())
 
-#sampler = greedy.SteepestDescentSolver()
+sampleset = sampler.sample(model, num_reads = runs, chain_strength = Kappa)
 
-sampleset = sampler.sample(model, num_reads = 1000, chain_strength = 8)
+#ANALYSIS OF SOLUTIONS STARTS HERE 
 
-print(sampleset.variables)
+#This prints out the energies of all solutions 
+
+all_solutions = len(sampleset.record)
+
+print("The total number of solutions found is:", all_solutions) 
+
+#for i in range(0,all_solutions):
+#    print(sampleset.record[i][1])
+
+#Establish the smallest energy in the sample of the solutions. 
+#This smallest energy can be achieved by different solutions, i.e. several equal minima.
+
+# Set some very high upper boundary for the energy 
+
+Energymax = 100000
+
+numoptsolutions = 0
+
+for i in range(0,all_solutions):
+    if(sampleset.record[i][1]<=Energymax):
+         Energymax = sampleset.record[i][1]
+
+print("The minimum energy found in these runs is")
+
+print(Energymax)
+    
+#Output all solutions that correspond to the minimum energy
+
+# Tol is the tolerance for differnces between minimum solutions due to numerics
+
+Tol = 1e-08
+for i in range(0,all_solutions):
+    if(abs(sampleset.record[i][1] - Energymax)<Tol):
+        print("Solution", numoptsolutions)
+        numoptsolutions = numoptsolutions + 1
+
+        print("The energy of this solution is", sampleset.record[i][1]) 
+        print("Density distribution")
+        for j in range(0,nodes+1):    
+          density_result[j] = b1*sampleset.record[i][0][j*3+0] + b2*sampleset.record[i][0][j*3+1] + b3*sampleset.record[i][0][j*3+2]
+          print(0.5*D + j*D, density_result[j])
+
 
 print("=======================")
 
-#total number of variables (nodes+1)*5
+#print(sampleset.record)
 
-for i in range(0,nodes+1):
-    density_result[i] = b1*sampleset.first[0][i*3+1] + b2*sampleset.first[0][i*3+2] + b3*sampleset.first[0][i*3+3]
-    print(0.5*D + i*D, density_result[i])
+#print("=======================")
+
+# Cross-check the previous output. 
+# The output below gives one minimum solution that must be part of the previous output
+
+# for i in range(0,nodes+1):
+# density_result[i] = b1*sampleset.first[0][i*3+1] + b2*sampleset.first[0][i*3+2] + b3*sampleset.first[0][i*3+3]
+# print(0.5*D + i*D, density_result[i])
 
 print("Calculate total mass in box")
 
 Mass = 0.
-Fenergy = 0
+#Fenergy = 0
 
 for i in range(0,nodes+1):
     Mass = Mass + density_result[i]*D
