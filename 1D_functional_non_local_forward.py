706b30de4b2006e7ada64a49f90a41b962bf85b0
Kostas Daoulas <daoulas@pckr207.mpip-mainz.mpg.de>
2023-03-14 13:23:56 +0100

    rewrote 1D functional non local forward

--- 1D_functional_non_local_forward.py
+++ 1D_functional_non_local_forward.py
@@ -18,6 +18,8 @@ density_result = {}
 
 nodes =  5
 
+runs = 5000
+
 #largest value of p
 
 h = 1
@@ -37,6 +39,17 @@ u = 5.
 
 D2 = D*D 
 #
+
+Kappa = 5.
+
+Coeff = (Kappa/2.)*(1/D)
+
+# units of p[i]: ~1/Length
+# units of Kappa: energy*Length^3
+# units of Coeff: energy*Length^2
+
+print("Coeff = ", Coeff)
+
 po2 = po*po
 
 b1 = h/7
@@ -44,7 +57,7 @@ b2 = 2*h/7
 b3 = 4*h/7
 
 #Weight coeffs for constraints
-Lan2 = 3.5
+Lan2 = 1.5
 
 #define powers 
 
@@ -61,9 +74,9 @@ b3p3 = math.pow(b3,3)
 b3p4 = math.pow(b3,4)
 
 for i in range(0,nodes+1):
-      Q[1+i*5,2+i*5] = (2*b1p3*b2*u + 3*b1p2*b2p2*u - 3*b1p2*b2*u*po + 2*b1*b2p3*u - 3*b1*b2p2*u*po + b1*b2*po2*u)*D + Lan2*2
-      Q[1+i*5,3+i*5] = (2*b1p3*b3*u + 3*b1p2*b3p2*u - 3*b1p2*b3*u*po + 2*b1*b3p3*u - 3*b1*b3p2*u*po + b1*b3*po2*u)*D + Lan2*2
-      Q[2+i*5,3+i*5] = (2*b2p3*b3*u + 3*b2p2*b3p2*u - 3*b2p2*b3*u*po + 2*b2*b3p3*u - 3*b2*b3p2*u*po + b2*b3*po2*u)*D + Lan2*2
+      Q[1+i*5,2+i*5] = (2*b1p3*b2*u + 3*b1p2*b2p2*u - 3*b1p2*b2*u*po + 2*b1*b2p3*u - 3*b1*b2p2*u*po + b1*b2*po2*u)*D + Lan2*2 + 4.*b1*b2*Coeff
+      Q[1+i*5,3+i*5] = (2*b1p3*b3*u + 3*b1p2*b3p2*u - 3*b1p2*b3*u*po + 2*b1*b3p3*u - 3*b1*b3p2*u*po + b1*b3*po2*u)*D + Lan2*2 + 4.*b1*b3*Coeff
+      Q[2+i*5,3+i*5] = (2*b2p3*b3*u + 3*b2p2*b3p2*u - 3*b2p2*b3*u*po + 2*b2*b3p3*u - 3*b2*b3p2*u*po + b2*b3*po2*u)*D + Lan2*2 + 4.*b2*b3*Coeff
       Q[1+i*5,4+i*5] = -Lan2*2
       Q[1+i*5,5+i*5] = -Lan2*4
       Q[2+i*5,4+i*5] = -Lan2*2
@@ -72,18 +85,38 @@ for i in range(0,nodes+1):
       Q[3+i*5,5+i*5] = -Lan2*4
       Q[4+i*5,5+i*5] = (6*b1p2*b2*b3*u + 6*b1*b2p2*b3*u + 6*b1*b2*b3p2*u - 6*b1*b2*b3*po*u)*D + Lan2*4
 
+for i in range(0,nodes):
+     Q[1+i*5,1+(i+1)*5] = -2*b1p2*Coeff
+     Q[1+i*5,2+(i+1)*5] = -2*b1*b2*Coeff
+     Q[2+i*5,1+(i+1)*5] = -2*b1*b2*Coeff
+     Q[1+i*5,3+(i+1)*5] = -2*b1*b3*Coeff
+     Q[3+i*5,1+(i+1)*5] = -2*b1*b3*Coeff
+     Q[2+i*5,2+(i+1)*5] = -2*b2p2*Coeff
+     Q[2+i*5,3+(i+1)*5] = -2*b2*b3*Coeff
+     Q[3+i*5,2+(i+1)*5] = -2*b2*b3*Coeff
+     Q[3+i*5,3+(i+1)*5] = -2*b3p2*Coeff
+
+Q[1+0*5,1+nodes*5] = -2*b1p2*Coeff
+Q[1+0*5,2+nodes*5] = -2*b1*b2*Coeff
+Q[2+0*5,1+nodes*5] = -2*b1*b2*Coeff
+Q[1+0*5,3+nodes*5] = -2*b1*b3*Coeff
+Q[3+0*5,1+nodes*5] = -2*b1*b3*Coeff
+Q[2+0*5,2+nodes*5] = -2*b2p2*Coeff
+Q[2+0*5,3+nodes*5] = -2*b2*b3*Coeff
+Q[3+0*5,2+nodes*5] = -2*b2*b3*Coeff
+Q[3+0*5,3+nodes*5] = -2*b3p2*Coeff
 
 for i in range(0,nodes+1):
-        V[1+i*5] = (-b1p3*u*po + 0.5*b1p2*u*po2 + 0.5*b1p4*u) + Lan2 - chempot*D*b1
-        V[2+i*5] = (-b2p3*u*po + 0.5*b2p2*u*po2 + 0.5*b2p4*u) + Lan2 - chempot*D*b2
-        V[3+i*5] = (-b3p3*u*po + 0.5*b3p2*u*po2 + 0.5*b3p4*u) + Lan2 - chempot*D*b3
+        V[1+i*5] = (-b1p3*u*po + 0.5*b1p2*u*po2 + 0.5*b1p4*u) + Lan2 - chempot*D*b1 + Coeff*2.*b1p2
+        V[2+i*5] = (-b2p3*u*po + 0.5*b2p2*u*po2 + 0.5*b2p4*u) + Lan2 - chempot*D*b2 + Coeff*2.*b2p2
+        V[3+i*5] = (-b3p3*u*po + 0.5*b3p2*u*po2 + 0.5*b3p4*u) + Lan2 - chempot*D*b3 + Coeff*2.*b3p2
         V[4+i*5] = Lan2
         V[5+i*5] = 4*Lan2
 
 print("Coupling coefficients")
 
 for i in range(1,6):
-    for j in range(i+1,6):
+    for j in range(1,6):
         print(i, j, Q[i,j])
  
 for i in range(1,6):
@@ -95,46 +128,83 @@ vartype = dimod.BINARY
 
 model = dimod.BinaryQuadraticModel(V, Q, offset, vartype)
 
-sampler = EmbeddingComposite(DWaveSampler())
 
-#sampler = greedy.SteepestDescentSolver()
+#This is the old way I used:
 
-sampleset = sampler.sample(model, num_reads = 1000, chain_strength = 15)
+#sampler = EmbeddingComposite(DWaveSampler())
 
-print(sampleset.variables)
+#New way for calling the sampler
+
+qpu_advantage = DWaveSampler()
+
+sampler = EmbeddingComposite(qpu_advantage)
+
+print("Maximum available annealing time:")
+print(qpu_advantage.properties["annealing_time_range"][1])
+
+long_time = qpu_advantage.properties["annealing_time_range"][1]*0.4
+
+long_time = 40
+
+sampleset = sampler.sample(model, num_reads = runs, chain_strength = Kappa, annealing_time=long_time)
+
+#sampleset = sampler.sample(model, num_reads = runs, chain_strength = Kappa)
+
+#ANALYSIS OF SOLUTIONS STARTS HERE 
+
+#This prints out the energies of all solutions 
+
+all_solutions = len(sampleset.record)
+
+print("The total number of solutions found is:", all_solutions)
+
+#for i in range(0,all_solutions):
+#    print(sampleset.record[i][1])
+
+#Establish the smallest energy in the sample of the solutions. 
+#This smallest energy can be achieved by different solutions, i.e. several equal minima.
+
+# Set some very high upper boundary for the energy 
+
+Energymax = 100000
+
+numoptsolutions = 0
+
+for i in range(0,all_solutions):
+    if(sampleset.record[i][1]<=Energymax):
+         Energymax = sampleset.record[i][1]
+
+print("The minimum energy found in these runs is")
+
+print(Energymax)
+
+#Output all solutions that correspond to the minimum energy
+
+# Tol is the tolerance for differnces between minimum solutions due to numerics
+
+Tol = 1e-08
+for i in range(0,all_solutions):
+    if(abs(sampleset.record[i][1] - Energymax)<Tol):
+        print("Solution", numoptsolutions)
+        numoptsolutions = numoptsolutions + 1
+
+        print("The energy of this solution is", sampleset.record[i][1])
+        print("Density distribution")
+        for j in range(0,nodes+1):
+          density_result[j] = b1*sampleset.record[i][0][j*5+0] + b2*sampleset.record[i][0][j*5+1] + b3*sampleset.record[i][0][j*5+2]
+          print(0.5*D + j*D, density_result[j])
+
+        print("Testing constraint between q1,q2,q3 and q4,q5")
+
+        for k in range(0,nodes+1):
+           print("node:", k, "constraint value:", sampleset.record[0][k*5+1]+sampleset.record[0][k*5+2]+sampleset.record[0][k*5+3]-
+           sampleset.record[0][k*5+4]-2*sampleset.record[0][k*5+5])
 
 print("=======================")
 
-#total number of variables (nodes+1)*5
-
-for i in range(0,nodes+1):
-    density_result[i] = b1*sampleset.first[0][i*5+1] + b2*sampleset.first[0][i*5+2] + b3*sampleset.first[0][i*5+3]
-    print(0.5*D + i*D, density_result[i])
-
-print("Calculate total mass in box")
-
-Mass = 0.
-Fenergy = 0
-
-for i in range(0,nodes+1):
-    Mass = Mass + density_result[i]*D
-    Fenergy = Fenergy +0.5*u*density_result[i]*(density_result[i]-po)*density_result[i]*(density_result[i]-po)*D
-
-print("Mass = ", Mass)
-
-print("Free energy = ", Fenergy - chempot*Mass)
-
-print("Testing constraint between q1,q2,q3 and q4,q5")
-
-for i in range(0,nodes+1):
-    print("node:", i, "constraint value:", sampleset.first[0][i*5+1]+sampleset.first[0][i*5+2]+sampleset.first[0][i*5+3]-
-          sampleset.first[0][i*5+4]-2*sampleset.first[0][i*5+5])
-        
-#  print(i,sampleset.first[0][i*5+4]+sampleset.first[0][i*5+5])
- 
-print("======") 
-
 dwave.inspector.show(sampleset)
+
+print(sampleset.info["timing"])
 
 #for i in range(1,(nodes+1)*5+1):
 #    print(sampleset.first[0][i])
